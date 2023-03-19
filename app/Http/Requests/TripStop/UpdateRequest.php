1fbd997fa52d7d2481531b8dd861424d6d42f382
AbdELRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2023-03-20 00:32:01 +0200

    Requests - Implemented Trip Stop validations

---
+++ app/Http/Requests/TripStop/UpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\TripStop;
+
+use App\Models\Trip;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $trip = Trip::find($this->trip_id);
+        $start_station_id = $trip->start_station_id;
+        $end_station_id = $trip->end_station_id;
+
+        return [
+            'trip_id' => 'required|exists:trips,id',
+            'station_id' => ['required',Rule::notIn([$start_station_id, $end_station_id])]
+        ];
+    }
+}
