e158d9de008805e48fe45965e80991862135eaad
AbdELRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2023-03-19 23:14:47 +0200

    Requests - Added & implemented Trip validations

---
+++ app/Http/Requests/Trip/StoreRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Trip;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:250',
+            'start_station_id' => 'required|exists:stations,id',
+            'end_station_id' => 'required|exists:stations,id',
+            'bus_id' => 'required|exists:buses,id'
+        ];
+    }
+}
