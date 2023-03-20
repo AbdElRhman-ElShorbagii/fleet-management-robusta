9fa6c7a7052f0cf9f8d3e5c9cc3bd54325641e7b
AbdELRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2023-03-20 15:37:20 +0200

    Update - Finished checking seats logic

--- app/Repositories/ReservationRepository.php
+++ app/Repositories/ReservationRepository.php
@@ -17,17 +17,28 @@ class ReservationRepository implements ReservationRepositoryInterface
         $available_seats = $trip->bus->available_seats;
         $full_trip_reservations = Reservation::where('trip_id',$trip_id)->where('from_station_id',$trip->start_station_id)->where('to_station_id',$trip->end_station_id)->pluck('seat_id');
         $full_trip_reservations_arr = json_decode($full_trip_reservations);
+        $trip_stop_from_station_id = TripStop::where('trip_id',$trip_id)->where('station_id',$from_station_id)->first();
+        $trip_stop_to_station_id = TripStop::where('trip_id',$trip_id)->where('station_id',$to_station_id)->first();
 
         // $full_reservations_count = Reservation::where('trip_id',$trip->id)->where('from_station_id',$trip->start_station_id)->where('to_station_id',$trip->end_station_id)->count();
 
         $is_going_from_start_to_end_station = Trip::where('id',$trip_id)->where('start_station_id',$from_station_id)->where('end_station_id',$to_station_id)->first();
-        // dd($is_going_from_start_to_end_station);
 
-        $is_going_from_start_to_stop_station = $trip->stops->where('station_id',$to_station_id)->first();
-        // dd($is_going_from_start_to_stop_station);
+        $is_going_from_start = Trip::where('id',$trip_id)->where('start_station_id',$from_station_id)->first();
+        $is_going_to_stop_station = $trip->stops->where('station_id',$to_station_id)->first();
+        if($is_going_from_start && $is_going_to_stop_station){
+            $is_going_from_start_to_stop_station = true;
+        }else{
+            $is_going_from_start_to_stop_station = false;
+        }
 
-        $is_going_from_stop_to_end_station = $trip->stops->where('station_id',$from_station_id)->first();
-        // dd($is_going_from_stop_to_end_station);
+        $is_going_from_stop = $trip->stops->where('station_id',$from_station_id)->first();
+        $is_going_to_end_station = Trip::where('id',$trip_id)->where('end_station_id',$to_station_id)->first();
+        if($is_going_from_stop && $is_going_to_end_station){
+            $is_going_from_stop_to_end_station = true;
+        }else{
+            $is_going_from_stop_to_end_station = false;
+        }
 
         $from_stop = $trip->stops->where('station_id',$from_station_id)->first();
         $to_stop = $trip->stops->where('station_id',$to_station_id)->first();
@@ -37,18 +48,21 @@ class ReservationRepository implements ReservationRepositoryInterface
         else{
             $is_going_from_stop_to_stop = false;
         }
-        // dd($is_going_from_stop_to_stop);
 
 
         if($is_going_from_start_to_end_station){
-            $reserved_seats = Reservation::where('trip_id',$trip_id)->pluck('seat_id');
-            $open_seats = Seat::whereNotIn('id',$reserved_seats)->get();
+            $reserved_seats = Reservation::where('trip_id',$trip_id);
+            $check_availability = $reserved_seats->count() >=  $available_seats;
+            if($check_availability){
+                return "No available seats to book";
+            }
+            $open_seats = Seat::whereNotIn('id',$reserved_seats->pluck('seat_id'))->get();
 
         }
         elseif($is_going_from_start_to_stop_station){
             $reserved_seats = Reservation::where('trip_id',$trip_id)
             ->where('from_station_id',$from_station_id)
-            ->where('to_station_id',$to_station_id)
+            ->orWhere('to_station_id','<',$trip_stop_to_station_id->id)
             ->pluck('seat_id');
 
             $reserved_seats_arr = json_decode($reserved_seats);
@@ -57,17 +71,33 @@ class ReservationRepository implements ReservationRepositoryInterface
         }
         elseif($is_going_from_stop_to_end_station){
             $reserved_seats = Reservation::where('trip_id',$trip_id)
-            ->where('from_station_id',$from_station_id)
+            ->orWhere('from_station_id','>',$trip_stop_from_station_id->id)
             ->where('to_station_id',$to_station_id)
             ->pluck('seat_id');
 
             $reserved_seats_arr = json_decode($reserved_seats);
             $merged_seats = array_merge($full_trip_reservations_arr, $reserved_seats_arr);
             $open_seats = Seat::whereNotIn('id',$merged_seats)->get();
-            return $open_seats;
         }
         elseif($is_going_from_stop_to_stop){
 
+            $trip_stops = TripStop::where('trip_id',$trip_id)
+            ->where('id','<',$trip_stop_from_station_id->id)
+            ->where('station_id','!=',$from_station_id)
+            ->pluck('station_id');
+            
+            $reserved_seats = Reservation::where('trip_id',$trip_id)
+            ->where('from_station_id',$from_station_id)
+            ->whereNotIn('to_station_id',$trip_stops)
+            ->pluck('seat_id');
+
+            $reserved_seats_arr = json_decode($reserved_seats);
+            $merged_seats = array_merge($full_trip_reservations_arr, $reserved_seats_arr);
+            $open_seats = Seat::whereNotIn('id',$merged_seats)->get();
+
+        }
+        else{
+            return "No available seats to book";
         }
         return $open_seats;
     }
